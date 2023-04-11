acdb18712fe49667ad0f0051fc8e0bc94bf81300
Alexandre Souza <907751+AlexzSouz@users.noreply.github.com>
2023-04-11 14:35:33 +0200

    Adding enums.go (#2)
    * Refactor imports to use the new module path and update the Go module to reflect the change
    
    * Adding enums.go
    
    ---------
    
    Co-authored-by: AlexzSouz <{ID}+{username}@users.noreply.github.com>

--- fluent/abstractions.go
+++ fluent/abstractions.go
@@ -19,28 +19,3 @@ type IComparable[T any] interface{}
 type IValidator[TValue any, TExpected any] interface {
 	Validate(t *testing.T, value TValue, expected TExpected, messagesf ...string)
 }
-
-/*
- * Enums
- */
-type MemberType int64
-
-const (
-	MemberTypeAll    MemberType = 0
-	MemberTypeField  MemberType = 1
-	MemberTypeMethod MemberType = 2
-)
-
-type AdditiveInverse bool
-
-const (
-	NegativeDefault AdditiveInverse = false
-	NegativeInvert  AdditiveInverse = true
-)
-
-type Match int
-
-const (
-	MatchAny Match = 1
-	MatchAll Match = 2
-)
